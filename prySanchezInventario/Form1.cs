e12c832dc58ed29a9a2dde0c182af2dfa219c04b
sagustineugenio-lgtm <sagustineugenio@gmail.com>
2026-04-06 09:57:32 -0300

    variables y boton de guardado

--- prySanchezInventario/Form1.cs
+++ prySanchezInventario/Form1.cs
@@ -11,7 +11,13 @@ using System.Windows.Forms;
 namespace prySanchezInventario
 {
     public partial class Form1 : Form
-    {
+    {   
+        string varCodigo ="";
+        string varProducto ="";
+        int varPrecio = 0;
+        int varStock= 0;
+        string varDescripcion = "";
+        string varCategoria = "";
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +31,12 @@ namespace prySanchezInventario
         private void btnGuardar_Click(object sender, EventArgs e)
         {
             lblRegistroID.Text = txtCodigo.Text + "/" + txtProducto.Text + "/" + "$" + txtPrecio.Text +"/" + txtStock.Text;
+                varCodigo = txtCodigo.Text;
+                varProducto = txtProducto.Text;
+                varPrecio = Convert.ToInt32(txtPrecio.Text);
+                varStock = Convert.ToInt32(txtStock.Text);
+                varDescripcion = txtDescripcion.Text;
+                varCategoria = lstCategoria.Text;
         }
     }
 }
